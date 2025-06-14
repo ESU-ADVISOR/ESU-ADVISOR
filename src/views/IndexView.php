a4c892bbe6b5eb5236df6eaddabd3f7089dad5f6
GiacomoLoat <giacomo.loat@studenti.unipd.it>
2025-06-14 23:48:34 +0200

    Piccolo fix aiuti alla navigazione, corretto aria-describedby tabella informazioni mensa, close #89

--- src/views/IndexView.php
+++ src/views/IndexView.php
@@ -87,7 +87,7 @@ class IndexView extends BaseView
             ];
 
             $menseInfoContent .= "
-                <p id='orari-mensa-description-" . htmlspecialchars(str_replace(' ', '-', strtolower($mensaId))) . "'>Tabella degli orari della mensa organizzata in due colonne: la prima indica i giorni della settimana, la seconda gli orari di apertura. Ogni riga corrisponde a un giorno.</p>
+                <p class='sr-only' id='orari-mensa-description-" . htmlspecialchars(str_replace(' ', '-', strtolower($mensaId))) . "'>Tabella degli orari della mensa organizzata in due colonne: la prima indica i giorni della settimana, la seconda gli orari di apertura. Ogni riga corrisponde a un giorno.</p>
                 <table aria-describedby=\"orari-mensa-description-" . htmlspecialchars(str_replace(' ', '-', strtolower($mensaId))) . "\">
                     <caption>Orari:</caption>
                     <thead>
